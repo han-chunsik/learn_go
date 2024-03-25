dff92981839833f4e697c930eeaf93ec049e5ed0
한춘식 <163226671+han-chunsik@users.noreply.github.com>
2024-03-25 22:16:44 +0900

    원복 helloworld

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,46 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	writer := bufio.NewWriter(os.Stdout)
-
-	var a int
-	fmt.Fscanln(reader, &a)
-
-	inputNums, _ := reader.ReadString('\n')
-
-	strSlice := strings.Split(strings.TrimSpace(inputNums), " ")
-	intSlice := make([]int, 0, a)
-
-	for _, s := range strSlice {
-		num, err := strconv.Atoi(s)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		intSlice = append(intSlice, num)
-	}
-
-	var b int
-	fmt.Fscanln(reader, &b)
-
-	count := 0
-
-	for i := 0; i < a; i++ {
-		if intSlice[i] == b {
-			count += 1
-		}
-	}
-
-	fmt.Fprint(writer, count)
-
-	writer.Flush()
+	fmt.Println("Hello world!")
 }

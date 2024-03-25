234debb05deee08e535228fc8ea723a84d1beb56
han-chunsik <vpdrnls96@gmail.com>
2024-03-25 22:14:02 +0900

    Go: The Complete Developer's Guide (Golang)

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	var a int
+	fmt.Fscanln(reader, &a)
+
+	inputNums, _ := reader.ReadString('\n')
+
+	strSlice := strings.Split(strings.TrimSpace(inputNums), " ")
+	intSlice := make([]int, 0, a)
+
+	for _, s := range strSlice {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		intSlice = append(intSlice, num)
+	}
+
+	var b int
+	fmt.Fscanln(reader, &b)
+
+	count := 0
+
+	for i := 0; i < a; i++ {
+		if intSlice[i] == b {
+			count += 1
+		}
+	}
+
+	fmt.Fprint(writer, count)
+
+	writer.Flush()
 }
